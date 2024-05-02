046de74aee726c296944eb85fd3c46fb0ce4b564
pwh <3025550698@qq.com>
2024-05-02 16:48:14 +0800

    feat:实现倒排索引添加文档

--- internal/reverse_index/skiplist_reverse_index.go
+++ internal/reverse_index/skiplist_reverse_index.go
@@ -1,7 +1,11 @@
 package reverse_index
 
 import (
+	"Research/types"
 	"Research/util"
+	"github.com/huandu/skiplist"
+	farmhash "github.com/leemcloughlin/gofarmhash"
+	"runtime"
 	"sync"
 )
 
@@ -9,4 +13,46 @@ import (
 type SkipListReverseIndex struct {
 	table *util.ResearchMap //分段map，并发安全
 	locks []sync.RWMutex    //修改倒排索引时，相同的key需要去竞争同一把锁
+}
+
+type SkipListValue struct {
+	Id          string
+	BitsFeature *util.Bitmap
+}
+
+// DocNumEstimate是预估的doc数量
+func NewSkipListReverseIndex(DocNumEstimate int) *SkipListReverseIndex {
+	indexer := new(SkipListReverseIndex)
+	indexer.table = util.NewResearchMap(runtime.NumCPU(), DocNumEstimate) // 分片数量为cpu数量
+	indexer.locks = make([]sync.RWMutex, 1000)
+	return indexer
+}
+
+// 根据key获取对应锁
+func (indexer SkipListReverseIndex) getLock(key string) *sync.RWMutex {
+	n := int(farmhash.Hash32WithSeed([]byte(key), 0))
+	return &indexer.locks[n%len(indexer.locks)]
+}
+
+// 添加文档
+func (m *SkipListReverseIndex) Add(doc types.Document) {
+	for _, keyWord := range doc.Keywords {
+		key := keyWord.ToString()
+		//对可能相同的key加锁
+		lock := m.getLock(key)
+		lock.Lock()
+
+		skipListValue := &SkipListValue{Id: doc.ID, BitsFeature: doc.BitsFeature}
+		if val, exist := m.table.Get(key); !exist {
+			//不存在，加入key，创建跳表
+			skipList := skiplist.New(skiplist.Uint64)
+			skipList.Set(doc.IntId, skipListValue)
+			m.table.Set(key, skipList)
+		} else {
+			//存在，获得跳表加入doc
+			skipList := val.(*skiplist.SkipList)
+			skipList.Set(doc.IntId, skipListValue)
+		}
+		lock.Unlock()
+	}
 }
